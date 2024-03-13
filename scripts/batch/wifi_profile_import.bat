4031aad0f7aa7b233cc17e41a8c91a5560161d96
uchithr <133994110+uchithr@users.noreply.github.com>
2024-03-13 10:11:28 +0530

    Add files via upload

---
+++ scripts/batch/wifi_profile_import.bat
@@ -0,0 +1,11 @@
+@echo off
+
+echo ## Deleting CRYSTAL-MARTIN ##
+netsh wlan delete profile name="CRYSTAL-MARTIN"
+
+
+echo ## Adding Wi-Fi profile... ##
+netsh wlan add profile filename = "\\CMSLWEB\Chathuranga-IT\zwi\CM.xml"
+
+
+pause
