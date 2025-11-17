dfbc6c400f4ba920e0a22c37eff134875a9c7555
uchithr <133994110+uchithr@users.noreply.github.com>
2025-11-17 16:45:08 +0530

    code update

--- scripts/python/_pip.py
+++ scripts/python/_pip.py
@@ -5,3 +5,14 @@ pip freeze > requirements.txt
 
 #RESTORE THE PACKAGES LATER
 pip install -r requirements.txt
+
+
+
+
+
+
+#If you have multiple Python versions
+py -3.11 -m pip list
+py -3.11 -m pip freeze
+pip3 list
+
